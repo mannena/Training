2ed22fac39ce2dc00feb6270c1787a56a2613e99
Ameni <49406451+mannena@users.noreply.github.com>
2022-03-03 11:17:53 +0100

    Update GbsuFtbLaiService.kt

--- gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/services/GbsuFtbLaiService.kt
+++ gbsuftblai-back/src/main/kotlin/com/kata/gbsuftblai/services/GbsuFtbLaiService.kt
@@ -4,9 +4,49 @@ import org.springframework.stereotype.Component
 
 @Component
 class GbsuFtbLaiService {
+    //function qui permet de convertir un number to list of digits
+    fun numberToArray(number :Int) :ArrayList<Int>{
+    var numberToTest=number;
+    val list = arrayListOf<Int>()
+    while (numberToTest != 0) {
+        list.add(numberToTest % 10);
+         numberToTest=numberToTest /10;
+        
+    }
+    list.reverse();
+    return list;
+}
 
     fun convertNumber(inputNumber: Int): String {
-        return ""
-    }
+    var result =  String();
+	if (inputNumber %3 ==0 )
+	 {
+		result+="Gbsu";
+	 }
+	 if (inputNumber %5 ==0 )
+	 {
+		 result+="Ftb";
+	 }
+	for (d in numberToArray(inputNumber)) {	
+			if (d ==3) {
+				result+="Gbsu";
+			}
+			
+			if (d ==5){
+				result+="Ftb";
+			}
+			if (d == 7) {
+				result+="Lai";
+			}
+
+	}
+	if (result.isEmpty())
+	{
+		result=inputNumber.toString();
+	}
+	return result;
+
+	}
+
 
 }
